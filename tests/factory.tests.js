0c2e120f448ad156843217b37d5eec9013c953c6
Nathri <nathrijacobs@gmail.com>
2018-10-08 15:09:25 +0200

    travis setup

--- tests/factory.tests.js
+++ tests/factory.tests.js
@@ -4,7 +4,7 @@ const dbfactory = require('../src/db-factory.js');
 const pg = require('pg');
 
 // const connectionString = process.env.DATABASE_URL || 'postgresql://coder:pg123@localhost:5432/reg_numbers';
-const connectionString = process.env.DATABASE_URL || 'postgresql://localhost:5432/codex';
+const connectionString = process.env.DATABASE_URL || 'postgresql://localhost:5432/reg_numbers';
 const Pool = pg.Pool;
 const pool = new Pool({
     connectionString
